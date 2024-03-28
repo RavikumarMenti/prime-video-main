60d51e2d5e886a8ce08ade495e32d0a51f78ae95
ravikumar9095 <ravikumar.m.9095@gmail.com>
2024-03-28 09:22:27 +0530

    first commit

---
+++ src/components/MovieItem/index.js
@@ -0,0 +1,43 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
+// Write your code here
+import Popup from 'reactjs-popup'
+import ReactPlayer from 'react-player'
+import {IoMdClose} from 'react-icons/io'
+
+import 'reactjs-popup/dist/index.css'
+import './index.css'
+
+const MovieItem = props => {
+  const {movieDetails} = props
+  const {thumbnailUrl, videoUrl} = movieDetails
+
+  return (
+    <div>
+      <Popup
+        modal
+        trigger={
+          <img src={thumbnailUrl} alt="thumbnail" className="thumbnail-img" />
+        }
+        className="popup-container"
+      >
+        {close => (
+          <div className="modal-container">
+            <button
+              className="close-button"
+              type="button"
+              data-testid="closeButton"
+              onClick={() => close()}
+            >
+              <IoMdClose size={20} color="##231f20" />
+            </button>
+            <div className="video-url-container">
+              <ReactPlayer url={videoUrl} controls />
+            </div>
+          </div>
+        )}
+      </Popup>
+    </div>
+  )
+}
+
+export default MovieItem
